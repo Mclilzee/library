65fce2f59976ec8f603a2f774a47061dac4f3377
Mclilzee <mc.lilzee@hotmail.com>
2021-12-16 17:01:24 +0100

    Add functionality

--- library.js
+++ library.js
@@ -1,16 +1,44 @@
 let myLibrary = [];
 
-for (let i = 1; i <= 10; i++) {
-  addBookToLibrary(i, i, i * 100);
-}
-
 updateBooks();
 
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = false;
 }
+
+const input = document.querySelector(".input");
+input.addEventListener("submit", (field) => {
+  field.preventDefault();
+  const title = input.title.value;
+  const author = input.author.value;
+  const pages = input.pages.value;
+
+  const book = new Book(title, author, pages);
+
+  if (checkIfBookInLibrary(book)) {
+    alert("This book already in library");
+    return;
+  }
+
+  if (title && author && pages) {
+    addBookToLibrary(title, author, pages);
+  } else {
+    alert("Book need more information!");
+  }
+});
+
+document
+  .querySelector("#toggle-selected-read")
+  .addEventListener("click", () => {
+    toggleRead();
+  });
+
+document.querySelector("#remove-selected").addEventListener("click", () => {
+  removeSelected();
+});
 
 function addBookToLibrary(title, author, pages) {
   myLibrary.push(new Book(title, author, pages));
@@ -23,8 +51,12 @@ function updateBooks() {
   });
   for (let i = 0; i < myLibrary.length; i++) {
     const bookElement = createBookInfo(myLibrary[i], i);
-
     document.querySelector(".books").appendChild(bookElement);
+
+    if (myLibrary[i].read) {
+      document.getElementById(`read-${i}`).textContent = "Read";
+      bookElement.classList.add("read");
+    }
   }
 }
 
@@ -45,6 +77,9 @@ function createBookInfo(book, index) {
 
   bookEvent(bookElement, index);
   for (const info in book) {
+    if (info === "read") {
+      continue;
+    }
     const info_box = document.createElement("div");
     info_box.classList.add("info-box");
 
@@ -70,13 +105,54 @@ function createBookInfo(book, index) {
 
 function bookEvent(book, id) {
   book.addEventListener("click", () => {
-    book.classList.toggle("read");
-    const read = document.querySelector(`#read-${id}`);
+    book.classList.toggle("selected");
+  });
+}
 
-    if (book.classList.contains("read")) {
+function toggleRead() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let bookElement = document.getElementById(i);
+    if (bookElement.classList.contains("selected")) {
+      bookElement.classList.toggle("read");
+      myLibrary[i].read = true;
+    }
+
+    bookElement.classList.remove("selected");
+
+    const read = document.querySelector(`#read-${i}`);
+
+    if (bookElement.classList.contains("read")) {
       read.textContent = "Read";
     } else {
       read.textContent = "";
     }
+  }
+}
+
+function removeSelected() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = document.getElementById(i);
+    if (book.classList.contains("selected")) {
+      delete myLibrary[i];
+    }
+  }
+
+  myLibrary = myLibrary.filter((element) => element);
+
+  updateBooks();
+}
+
+function checkIfBookInLibrary(newBook) {
+  let exist = false;
+  myLibrary.forEach((book) => {
+    if (
+      book.title === newBook.title &&
+      book.author === newBook.author &&
+      book.pages === newBook.pages
+    ) {
+      exist = true;
+    }
   });
+
+  return exist;
 }
